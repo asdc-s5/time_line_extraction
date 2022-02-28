63cb094fe3a504c5215726361c1185ca1193448e
asdc_s5 <alesjandro98@gmail.com>
2022-02-28 13:59:53 +0100

    Extraction of events included

--- get_char.py
+++ get_char.py
@@ -7,6 +7,9 @@ import re
 import os
 import csv
 
+from xml.etree import cElementTree as ET
+
+#------GLOBAL VARIABLES-----#
 FIELDS_E3C = ['file','string','begin', 'end', 'value', 'timex3Class'] 
 FILENAME_E3C = "time_expresions.csv"
 PATH = '/Users/asdc/Library/CloudStorage/OneDrive-UNED/E3C-Corpus-2.0.0/data_annotation/Spanish/layer1/'
@@ -17,27 +20,20 @@ PATH = '/Users/asdc/Library/CloudStorage/OneDrive-UNED/E3C-Corpus-2.0.0/data_ann
 #FICHERO TEXTO EXPRESIONES#
 #ESTRUCTURAR EL CÓDIGO CON MAIN Y MÉTODO PARA EXTRAER TEXTO Y EXPRESIONES DEL DATASET Y RESULTADOS DEL HEIDELTIME#
 
+    
+#------EXTRACT_TEXT-----#
+"""
+Extrae el texto clínico del XML
+Generalmente el id de la fila que contiene el texto tiene id=12, pero no siempre es así. 
+Lo que sí se cumple siempre es que el sofa de todas las filas indica el id de la fila con el texto.
+Por lo que se saca el sofa de la primera fila y se matchea con la fila con id=sofa.
+Por otra parte, en la fila con el texto, el texto no siempre ocupa la posición 0, también puede ser la 1. 
+Para solucionarlo se ejecuta un if por si la posición 0==None
+""" 
+def extract_text(file):
+    path = PATH + file
 
-
-def extraer(PATH, file):
-    PATH = PATH + file
-
-    #------EXTRAE LAS EXPRESIONES TEMPORALES DEL XML-----#
-    """
-    La clase 'timex3Class' puede tomar unos valores determinados por los autores.
-    """
-    df = pd.read_xml(PATH)
-    df_time = pd.DataFrame(df, columns= ['begin','end','timex3Class', 'value'])
-    df_time = df_time.loc[df_time['timex3Class'].isin(['DATE', 'TIME', 'DURATION', 'QUANTIFIER', 'SET', 'PREPOSTEXP', 'DOCTIME', 'DOCTIME'])]
-
-    #------EXTRAE EL TEXTO CLÍNICO DEL XML-----#
-    """
-    Generalmente el id de la fila que contiene el texto tiene id=12, pero no siempre es así. 
-    Lo que sí se cumple siempre es que el sofa de todas las filas indica el id de la fila con el texto.
-    Por lo que se saca el sofa de la primera fila y se matchea con la fila con id=sofa.
-    Por otra parte, en la fila con el texto, el texto no siempre ocupa la posición 0, también puede ser la 1. 
-    Para solucionarlo se ejecuta un if por si la posición 0==None
-    """
+    df = pd.read_xml(path)
     input_id = df.loc[df['id'] == 1]
     input_id = input_id['sofa']
     input_id = int(input_id.values[0])
@@ -45,18 +41,22 @@ def extraer(PATH, file):
     input_ = df.loc[df['id'] == input_id]
     input_ = input_['sofaString']
     if input_.values[0] == None:
-        input_clear = input_.values[1]#.replace("\n\t", "  ")
+        return input_.values[1]#.replace("\n\t", "  ")
     else: 
-        input_clear = input_.values[0]#.replace("\n\t", "  ")
+        return input_.values[0]#.replace("\n\t", "  ")
 
-    #------EXTRAE LAS EXPRESIONES TEMPORALES CORRESPONDIENTES AL TEXTO DEL XML-----#
-    """
-    Teniendo dos listas de pares, una 'begin' y otra 'end' que tienen el caracter inicial y final de cada expresión respecitavamente,
-    solo se recorre caracter a caracter cada pareja begin-end y lo guarda en un String 
-    """
+    
+#------EXTRACT_EXPRESIONS-----#
+"""
+Extrae las expresiones temporales correspondientes al texto del XML
+Teniendo dos listas de pares, una 'begin' y otra 'end' que tienen el caracter inicial y final de cada expresión respecitavamente,
+solo se recorre caracter a caracter cada pareja begin-end y lo guarda en un String 
+"""
+def extract_expresions(df_xml, input_clear):
+
     output_=[]
 
-    for begin, end in zip(df_time['begin'], df_time['end']):
+    for begin, end in zip(df_xml['begin'], df_xml['end']):
         texp = ""
         begin = int(begin)
         end = int(end)
@@ -66,8 +66,8 @@ def extraer(PATH, file):
         output_.append(texp)
 
 
-    #------ELIMINA LOS POSIBLES ESPACIOS FINALES Y SIGNOS DE PUNTUACIÓN DE LAS EXPRESIONES TEMPORALES-----#
     """
+    Elimina los posibles espacios finales y signos de puntuación de las expresiones temporales
     -Puede darse el caso en el que se cuente un espacio o un signo de puntuación después de la primera palabra.
     Por lo que se van a eliminar todos los signos de puntuación y espacios que no estén entre palabras.
     -La forma más sencilla de eliminar los espacios sobrantes es con la función split, aunque podría buscarse una re que fuera más rápida.
@@ -82,32 +82,71 @@ def extraer(PATH, file):
         clear_string = ' '.join(clear_string.split())
 
         output_clear.append(clear_string)
-    
+    return output_clear
 
-    write_csv(file, output_clear, df_time['begin'], df_time['end'], df_time['value'], df_time['timex3Class'])
-
-    #print("FILE:" + str(PATH))
-    #print("EXPRESIONES:" + str(output_clear))
-    #print("TEXTO:" + str(input_clear))    
-
-
-#------ESCRIBE EN UN CSV LAS EXPRESIONES TEMPORALES, SU VALOR, BEGIN, END Y CLASE, ASÍ COMO DE QUÉ FICHERO PROVIENEN-----#
+#------EXTRACT_EVENTS-----#
 """
-output_clear es una lista con todas las expresiones, por lo que hay que separarlas de 1 en 1
+Extrae los eventos del XML
+-Como el xml está ordenado por etiquetas y los eventos no tienen etiquetas distintivas sobre las que poder filtrar sobre otras,
+se ha utilizado otra librería para tratar el XML. Primero filtra por la etiqueta correspondiente a los eventos, que está definido en el XML
+(en la primera fila) y después filtra por las etiquetas correspondientes (begin, end, etc.), todo eso lo mete en un dataframe
+para poder pasarlo a un csv más tarde.
+"""
+def extract_events(file):
+    path = PATH + file
+
+    dfcols = ['begin', 'end']
+    df_event = pd.DataFrame(columns=dfcols)
+    root = ET.parse(path)
+
+    rows = root.findall('.//{http:///webanno/custom.ecore}EVENT')
+    for row in rows:
+        #print(row.attrib['begin'])
+        begin = row.attrib['begin']
+        end = row.attrib['end']
+        print(begin, end)
+        df_event = df_event.append(pd.Series([begin, end], index=dfcols), ignore_index=True)
+    
+    input_clear = extract_text(file)
+    output_clear = extract_expresions(df_event, input_clear)
+    print(output_clear)
+    #Faltaría que escribiera en un csv, pero ya se verá qué hace falta y si hace falta
+
+#------EXTRACT_TIMEX3-----#
+"""
+Extrae las expresiones temporales del XML
+-La clase que corresponde a las relaciones temporales tiene un campo específico, llamado timex3Class que puede tomar unos valores limitados, 
+determinados por los autores. Por lo que se puede utilizar esto para filtrar de un dataframe directamente sin tener que hacer el tratado
+que se hace para extraer los eventos.
+"""
+def extract_timex3(file):
+    path = PATH + file
+
+    df = pd.read_xml(path)
+    df_time = pd.DataFrame(df, columns= ['begin','end','timex3Class', 'value'])
+    df_time = df_time.loc[df_time['timex3Class'].isin(['DATE', 'TIME', 'DURATION', 'QUANTIFIER', 'SET', 'PREPOSTEXP', 'DOCTIME', 'DOCTIME'])]
+
+    input_clear = extract_text(file)
+    output_clear = extract_expresions(df_time, input_clear)
+
+    write_csv(file, output_clear, df_time['begin'], df_time['end'], df_time['value'], df_time['timex3Class']) 
+
+
+
+#------WRITE_CSV()-----#
+"""
+Escribe en un CSV las expresiones y distintos valores relacionados
+-output_clear es una lista con todas las expresiones, por lo que hay que separarlas de 1 en 1
 y emparejarlas con su begin, end, value y class, como vienen en orden no pasa nada.
 """
+#Cambiar begin, end, etc por una lista para poder reutilizarlo
 def write_csv(file, output_clear, begin, end, value, timex3Class):
     rows = []
-
+    
+    #Generates the rows for the csv
     for beg, end_, val, timex3, output in zip(begin, end, value, timex3Class, output_clear):
         row = [str(file), str(output), str(int(beg)), str(int(end_)), str(val), str(timex3)]
         rows.append(row)
-        #print("FILE:" + str(file))
-        #print("BEGINING:" + str(int(beg)))
-        #print("END:" + str(int(end_)))
-        #print("VALUE:" + str(val))
-        #print("TIMEX3CLASS:" + str(timex3))
-        #print("OUTPUT:" + str(output))
     # writing to csv file 
     with open(FILENAME_E3C, 'a') as csvfile: 
         #creating a csv writer object 
@@ -116,7 +155,10 @@ def write_csv(file, output_clear, begin, end, value, timex3Class):
         #writing the data rows 
         csvwriter.writerows(rows)
 
-
+#------WRITE_CSV()-----#
+"""
+Crea un csv según los parametros definidos en las constantes globales
+"""
 def create_csv():
 
     with open(FILENAME_E3C, 'w') as csvfile: 
@@ -130,8 +172,26 @@ def create_csv():
 
 def heidelTime():
     False
+    
 
+def main():
+
+    #create_csv()
+    #for file in  os.listdir(PATH):
+        #extract_timex3(PATH, file)
+    extract_events('ES100001.xml')
+
+    
+
+if __name__ == "__main__":
+    main()
+
+
+
+#------PRUEBAS------#
+"""
 def pruebas():
+
     PATH = '/Users/asdc/Library/CloudStorage/OneDrive-UNED/E3C-Corpus-2.0.0/data_annotation/Spanish/layer1/ES100688.xml'
     df = pd.read_xml(PATH)
     input_id = df.loc[df['id'] == 1]
@@ -147,25 +207,7 @@ def pruebas():
         input_clear = input_.values[0]#.replace("\n\t", "  ")
 
     print("TEXTO:" + str(input_clear))
-    
-    
-
-def main():
-
-    create_csv()
-    for file in  os.listdir(PATH):
-        extraer(PATH, file)
-    #pruebas()
-
-
-
-if __name__ == "__main__":
-    main()
-
-
-
-#------PRUEBAS------#
-
+"""
 #------ESTAS LÍNEAS VALEN SI SE EJECUTA EN PYTHON2.X------#
 #clear_input = clear_input.replace("ñ", "\xc3\xb1")
 #clear_input = clear_input.decode("utf-8")
