1361960fa4912dc025960a086b3a488b79ccb10d
asdc_s5 <alesjandro98@gmail.com>
2022-03-07 22:15:24 +0100

    Updates about heidel

---
+++ python_heideltime/python_heideltime/config_Heideltime.py
@@ -0,0 +1,4 @@
+# for now this stores only the path to heideltime-standalone
+#Heideltime_path = '/path/to/heideltime/'
+Heideltime_path = '/Users/asdc/Proyectos/time_line_extraction/python_heideltime/heideltime-standalone/heideltime-standalone/de.unihd.dbs.heideltime.standalone.jar'
+
